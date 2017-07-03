8611289d96a5c18bf41e4039ca62028ff7bce31a
Regner Blok-Andersen <shadowdf@gmail.com>
2017-07-03 15:22:14 -0700

    Removed extra comment

--- build/config.js
+++ build/config.js
@@ -4,7 +4,5 @@ const pkg = require('../package')
 module.exports = {
   port: 4000,
   title: 'slot list',
-  // when you use electron please set to relative path like ./
-  // otherwise only set to absolute path when you're using history mode
   publicPath: '/',
 }
