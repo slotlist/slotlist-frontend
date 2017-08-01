16fd0ccb780c387eab1597634e9279dd22653c9d
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2017-08-01 13:39:50 +0200

    Added table footer for mission- and slotlists with >= 10 entries

--- src/components/MissionSlotlistTable.vue
+++ src/components/MissionSlotlistTable.vue
@@ -13,6 +13,15 @@
       <tbody>
         <mission-slotlist-row v-for="slot in slotlist" :missionSlot="slot" :key="slot.id"></mission-slotlist-row>
       </tbody>
+      <tfoot v-show="slotlist.length >= 10">
+        <tr>
+          <th style="width: 1%">#</th>
+          <th style="width: 25%">Role</th>
+          <th style="width: 34%">Player</th>
+          <th style="width: 30%">Description</th>
+          <th style="width: 10%" class="text-center">Actions</th>
+        </tr>
+      </tfoot>
     </table>
   </div>
 </template>
--- src/components/MissionlistTable.vue
+++ src/components/MissionlistTable.vue
@@ -13,6 +13,15 @@
       <tbody>
         <missionlist-row v-for="mission in missions" :mission="mission" :key="mission.uid"></missionlist-row>
       </tbody>
+      <tfoot v-show="missions.length >= 10">
+        <tr>
+          <th style="width: 15%">Start time</th>
+          <th style="width: 25%">Title</th>
+          <th style="width: 35%">Description</th>
+          <th style="width: 10%">Creator</th>
+          <th style="width: 10%" class="text-center">Actions</th>
+        </tr>
+      </tfoot>
     </table>
   </div>
 </template>
