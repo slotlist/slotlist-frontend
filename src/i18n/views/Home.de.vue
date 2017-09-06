1e01057ce4c7578390a9815eb95e3c257ac91a6a
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2017-09-06 14:33:37 +0200

    Started translating static views (#6)
    Currently finished `Home` view

---
+++ src/i18n/views/Home.de.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-jumbotron header="slotlist.info" lead="ArmA 3 Missions- und Slotlist-Management">
+    <p class="text-justify">
+      <router-link :to="{name: 'home'}">slotlist.info</router-link> ermöglich das Erstellen und Verwalten von
+      <router-link :to="{name: 'missionList'}">Missionen</router-link> inklusive Slotlisten, bei denen Benutzer sich für ihre gewünschten Slots anmelden können.
+      <br>Das
+      <router-link :to="{name: 'communityList'}">Community</router-link> Feature erlaubt es Communities, ihre Gemeinschaft auf unserer Website zu präsentieren und dadurch weiter ihre Mitglieder und deren erstellte Missionen zu bewerben.
+      <br>Da
+      <router-link :to="{name: 'home'}">slotlist.info</router-link> Steam Single Sign On benutzt, wird keine lästige Registrierung und ein weiterer Account auf noch einer weiteren Seite benötigt - alle Authentifizierung wird sicher von den Steam Servern erledigt. Zum Loslegen einfach
+      <router-link :to="{name: 'login'}">anmelden</router-link> und
+      <router-link :to="{name:'missionCreator'}">eine neue Mission erstellen</router-link>!
+      <br>
+      <br>Weil sich
+      <router-link :to="{name: 'home'}">slotlist.info</router-link> noch immer in der Entwicklungsphase befindet, wird es immer wieder Änderungen an den Seiten und deren Features (oder Daten) geben. Wir werden unser Bestes geben, um Service-Ausfälle möglichst kurz zu halten und Datenverluste zu verhindern,
+      <router-link :to="{name: 'about'}">kontaktiere uns</router-link> aber bitte jederzeit, falls irgendwelche Probleme auftreten sollten oder es Anregungen gibt.
+      <br>
+      <br>Vielen Dank für die Nutzung unserer Services - ich hoffe, sie sind von gutem Nutzen!
+      <br>
+      <br>
+      <span class="text-muted font-italic">&mdash; MorpheusXAUT, slotlist.info Entwickler</span>
+    </p>
+  </b-jumbotron>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,21 @@
 <template>
   <div>
-    <b-jumbotron header="slotlist.info" lead="ArmA 3 mission planning and slotlist management tool">
-      <p class="text-justify">
-        <router-link :to="{name: 'home'}">slotlist.info</router-link> allows you to create and manage
-        <router-link :to="{name: 'missionList'}">missions</router-link>, including slotlists for users to register for their desired roles.
-        <br>The
-        <router-link :to="{name: 'communityList'}">communities</router-link> feature also allows you to represent your ArmA community on our website, further advertising your community's members and the missions created by them.
-        <br>Since
-        <router-link :to="{name: 'home'}">slotlist.info</router-link> uses Steam Single Sign on, there's no need for your to create yet another account on a different website - all authentication is securely handled by Steam servers. You can get started right away by
-        <router-link :to="{name: 'login'}">logging in</router-link> and
-        <router-link :to="{name:'missionCreator'}">creating a new mission</router-link>!
-        <br>
-        <br>Since
-        <router-link :to="{name: 'home'}">slotlist.info</router-link> is still in development, there will be ongoing changes to the sites and the features (or data) provided by it. We will do our best to keep downtimes low and prevent any data loss, but please
-        <router-link :to="{name: 'about'}">contact us</router-link> regarding any suggestions or issues - should they arise.
-        <br>
-        <br>Thank you for using our service - I hope you enjoy it!
-        <br>
-        <br>
-        <span class="text-muted font-italic">&mdash; MorpheusXAUT, slotlist.info developer</span>
-      </p>
-    </b-jumbotron>
+    <home-de v-if="$i18n.locale === 'de'"></home-de>
+    <home-en v-else></home-en>
   </div>
 </template>
 
 <script>
+import HomeDe from '../i18n/views/Home.de.vue'
+import HomeEn from '../i18n/views/Home.en.vue'
 import utils from '../utils'
 
 export default {
-  created: function () {
+  components: {
+    HomeDe,
+    HomeEn
+  },
+  created: function() {
     utils.clearTitle()
   }
 }
