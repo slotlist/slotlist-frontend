37a36a0f31ec7c9c5e27d4caf431fc05cac4cdce
Nick 'MorpheusXAUT' Mueller <nick@morpheusxaut.net>
2017-10-17 23:14:13 +0200

    Fixed cookieconsent loading issue caused by browser plugin block

---
+++ static/cookieconsent.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",function(){window.cookieconsent&&window.cookieconsent.initialise({palette:{popup:{background:"#edeff5",text:"#838391"},button:{background:"transparent",text:"#4b81e8",border:"#4b81e8"}},content:{href:"https://slotlist.info/#/privacy"}})});
